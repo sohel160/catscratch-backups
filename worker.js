cc8b97a17132ca8dfd1833c238d573783fb3ff91
sohel160 <sohelcmc53rd@gmail.com>
2026-08-31 12:11:23 +0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -38,52 +38,9 @@ proxies:
   - name: proxy1
     type: http
     server: 103.115.242.240
-    port: 2610
+    port: 
 
-  - name: proxy2
-    type: http
-    server: 103.172.15.9
-    port: 5452
-
-  - name: proxy3
-    type: http
-    server: 144.48.108.121
-    port: 5452
-
-  - name: proxy4
-    type: http
-    server: 144.48.108.122
-    port: 5452
-    
-  - name: proxy5
-    type: http
-    server: 103.172.15.21
-    port: 5452
-
-  - name: proxy6
-    type: http
-    server: 103.172.15.5
-    port: 5452
-
-  - name: proxy7
-    type: http
-    server: 103.172.15.17
-    port: 5452
-    
-  - name: proxy8
-    type: http
-    server: 103.172.14.5
-    port: 5452
-    
-  - name: proxy9
-    type: http
-    server: 103.172.14.253
-    port: 5452
-
-  - name: proxy10
-    type: http
-    server: 103.172.14.245
-    port: 5452
+  
     
 `
 
